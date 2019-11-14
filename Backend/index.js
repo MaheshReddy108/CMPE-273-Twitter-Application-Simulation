ed41f9b1f43d86e126f0b897cc8bcdcc61dfc09f
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2019-11-14 11:58:02 -0800

    Added Tweet apis
    Added Tweet apis

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-//Load User Model
-const Tweet = require("./models/tweet");
+var bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Mongoose DB config
 const db = require("./config/keys").mongoURI;
@@ -13,54 +14,16 @@ mongoose
   .then(() => console.log("MongoDB Connected from Mongoose"))
   .catch(err => console.log(err));
 
-/*var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb+srv://sruthiduvvuri:3690@mongocluster-ou2ug.mongodb.net/";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("twitterdb");
-  //Find the first document in the customers collection:
-  dbo
-    .collection("tweet")
-    .find({})
-    .toArray(function(err, result) {
-      if (err) throw err;
-      result.forEach(result => {
-        console.log(result.tweet_content);
-      });
-      db.close();
-    });
-}); */
+//defining routes
+const tweets = require("./routes/api/tweets");
 
 app.get("/", function(request, response) {
   console.log("Inside Node Home");
   response.send("Node Backend is working");
 });
 
-app.get("/getTweets", function(request, response) {
-  Tweet.find({})
-    .then(tweet => {
-      console.log("The tweets are " + tweet);
-      return response.status(200).json(tweet);
-    })
-    .catch(err => response.status(400).json(err));
-});
-
-app.post("/postTweet", function(request, response) {
-  console.log("the request is " + request);
-  var msg = "New tweet test";
-  const newTweet = new Tweet({
-    tweet_content: msg
-  });
-  newTweet
-    .save()
-    .then(tweet => {
-      console.log("the tweet inserted is" + tweet._id);
-      console.log("the tweet inserted is" + tweet.tweet_content);
-      return response.send("tweet inserted successfully");
-    })
-    .catch(err => console.log("The error is " + err));
-});
+//use routes
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 4500;
 app.listen(port, () => console.log(`Server running on port ${port}`));
