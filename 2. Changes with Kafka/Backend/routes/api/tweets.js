474fd4a98ecdb43757dd574a9e418cdb0cedc8a4
katturumanish <kasturimanish15@gmail.com>
2019-11-16 17:56:35 -0800

    Update tweets.js

--- 2. Changes with Kafka/Backend/routes/api/tweets.js
+++ 2. Changes with Kafka/Backend/routes/api/tweets.js
@@ -70,4 +70,43 @@ router.post("/create_tweet", (req, res) => {
     .catch(err => res.status(404).json({ error: "Tweet not found" }));
 });*/
 
+router.post('/reply/:id', passport.authenticate('jwt', { session: false}),(req,res)=>{
+  const { errors, isValid} = validatePostInput(req.body);
+
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+
+  Post.findById(req.params.id)
+    .then(post => {
+       const newReply = {
+         text: req.body.text,
+         name: req.body.name,
+         avatar: req.body.avatar,
+         user: req.user.id
+       }
+
+       post.replies.unshift(newReply);
+
+       post.save().then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+});
+
+router.post('/like/:id', passport.authenticate('jwt', { session: false}),(req,res)=>{
+  Profile.findOne({ user:req.user.id}).then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+          return res.status(400).json({ alreadyliked: 'User already liked this post'});
+        }
+        
+        post.likes.unshift({ user: req.user.id });
+
+        post.save().then(post => res.json(post));
+        
+      })
+  })
+});
+
 module.exports = router;
