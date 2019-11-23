45ed27c9b4cf73faaab68708a9f3393eec128dc6
katturumanish <kasturimanish15@gmail.com>
2019-11-22 18:51:25 -0800

    Creating the list is pushed

---
+++ 1. Backend without Kafka/routes/api/lists.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+//const uuidv4 = require("uuid/v4");
+var passport = require("passport");
+const multer = require("multer");
+const path = require("path");
+
+const lists = require("../../models/Lists");
+
+router.post("/create/" , (req, res) => {
+    const { listName, listDesc, isPrivate } = req.body;
+    if (!(listName && listDesc)) {
+      console.error("Required Details Missing");
+      return res.status(400).json({ message: "Required Details Missing" });
+    }
+    try {
+      const loggedInUser = req.user;
+      const newList = new lists({
+        ownerID: 3489584568945,
+        listName: listName,
+        listDesc: listDesc,
+        isPrivate: isPrivate
+      });
+      newList.save().then(list => res.status(200).json(list));
+    } catch (e) {
+        res.status(500).send(e.message || e);
+    }
+  });
+
+  
+module.exports = router;
