b28154bdf8bcb8b76d5d6546f60b1a8e6e475503
katturumanish <kasturimanish15@gmail.com>
2019-11-22 19:03:37 -0800

    Update lists.js

--- 1. Backend without Kafka/routes/api/lists.js
+++ 1. Backend without Kafka/routes/api/lists.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var router = express.Router();
+//const uuidv4 = require("uuid/v4");
 var passport = require("passport");
+const multer = require("multer");
 const path = require("path");
 
 const lists = require("../../models/Lists");
 
 router.post("/create", passport.authenticate("jwt", { session: false }) , (req, res) => {
-    const { listName, listDesc, isPrivate } = req.body;
-    if (!(listName && listDesc)) {
+    const { l_Name, l_Desc, is_Private } = req.body;
+    if (!(l_Name && l_Desc)) {
       console.error("Required Details Missing");
       return res.status(400).json({ message: "Required Details Missing" });
     }
@@ -15,9 +17,9 @@ router.post("/create", passport.authenticate("jwt", { session: false }) , (req, 
       const loggedInUser = req.user;
       const newList = new lists({
         ownerID: loggedInUser.userID,
-        listName: listName,
-        listDesc: listDesc,
-        isPrivate: isPrivate
+        list_Name: l_Name,
+        list_Desc: l_Desc,
+        make_Private: is_Private
       });
       newList.save().then(list => res.status(200).json(list));
     } catch (e) {
