166d44dbd30e9b8aecef6775b372f98924549231
katturumanish <kasturimanish15@gmail.com>
2019-11-24 14:57:39 -0800

    update lists schema

--- 1. Backend without Kafka/models/Lists.js
+++ 1. Backend without Kafka/models/Lists.js
@@ -3,15 +3,33 @@ const Schema = mongoose.Schema;
 
 const ListSchema = new Schema(
   {
-    listID: String,
-    list_Name: String,
-    ownerID: String,
-    subscribers: Array,
-    members: Array,
-    make_Private: String,
-    list_Desc: String
+   listID:String,
+   list_Name :String,
+   ownerID:String,
+   subscribers: [
+    {
+      user_id: {
+        type: String,
+      },
+      username: {
+        type: String
+      }
+    }
+  ],
+   members: [
+    {
+      user_id: {
+        type: String
+      },
+      username: {
+        type: String
+      }
+    }
+  ],
+   make_Private:String,
+   list_Desc:String
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
 module.exports = lists = mongoose.model("lists", ListSchema);
