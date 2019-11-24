b50bae1f5ef88b29815818f03e47d9abfdeb0c42
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2019-11-23 19:22:10 -0800

    Merging changes with Signup and Login
    Changes are made to frontend to correct authorization. Backend routes and index files are modified. Changes to package files

--- 1. Backend without Kafka/index.js
+++ 1. Backend without Kafka/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
