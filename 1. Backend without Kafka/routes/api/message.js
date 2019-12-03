0c58d57512376ebf7178b0281af364aa794964a0
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2019-12-02 16:00:02 -0800

    Cleaning up backend folder
    Cleaning up backend folder

--- 1. Backend without Kafka/routes/api/message.js
+++ 1. Backend without Kafka/routes/api/message.js
@@ -1,10 +1,6 @@
 const express = require("express");
 
 const router = express.Router();
-const passport = require("passport");
-const multer = require("multer");
-const path = require("path");
-const jwt = require("jsonwebtoken");
 
 const Message = require("../../models/Message");
 
@@ -17,13 +13,13 @@ router.post("/send_message", (req, res) => {
         console.log("no user");
         return res.status(404).json({ msg: "no sender with this id" });
       }
-      console.log("sender user details:", user);
+      // console.log("sender user details:", user);
       User.findOne({ username: receiver_name }).then(user1 => {
         if (!user1) {
           console.log("no receiver found");
           return res.status(404).json({ msg: "no receiver with this id" });
         }
-        console.log("receiver user details:", user1);
+        // console.log("receiver user details:", user1);
         const newMessage = new Message({
           sender_name: sender_name,
           receiver_name: receiver_name,
@@ -38,42 +34,6 @@ router.post("/send_message", (req, res) => {
     });
 });
 
-/* router.post("/get_sent_messages",  (req, res) => {
-    const { user_id } = req.body;
-    console.log("inside get_sent_messages api of backend");
-    Message.find({ sender_id: user_id })
-    .then(message => {
-      if (!message) {
-        console.log("no message");
-        return res.status(404).json({ msg: "no sent messages from this sender" });
-      }
-      console.log("message is....", message);
-      res.json(message);
-    })
-    .catch(err => {
-      console.log("err is.....", err);
-      res.status(404).json(err);
-    });
-  }); */
-
-/* router.post("/get_received_messages",  (req, res) => {
-    const { user_id } = req.body;
-    console.log("inside get_received_messages api of backend");
-    Message.findOne({ receiver_id: user_id })
-    .then(message => {
-      if (!message) {
-        console.log("no message");
-        return res.status(404).json({ msg: "no received messages to this receiver" });
-      }
-      console.log("message is....", message);
-      res.json(message);
-    })
-    .catch(err => {
-      console.log("err is.....", err);
-      res.status(404).json(err);
-    });
-  }); */
-
 router.post("/get_messages", (req, res) => {
   const { sender_name, receiver_name } = req.body;
   console.log("inside get_messages api of backend");
@@ -85,7 +45,7 @@ router.post("/get_messages", (req, res) => {
           .status(404)
           .json({ msg: "no sent messages from this sender" });
       }
-      console.log("message is....", message);
+      // console.log("message is....", message);
       const all_msgs = [];
       all_msgs.push(message);
       Message.find({
@@ -98,7 +58,7 @@ router.post("/get_messages", (req, res) => {
             .status(404)
             .json({ msg: "no received messages for this sender" });
         }
-        console.log("received messages are: ", msg1);
+        // console.log("received messages are: ", msg1);
         all_msgs.push(msg1);
         res.json(all_msgs);
       });
